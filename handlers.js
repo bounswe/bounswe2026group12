29830fb1ba29f18cf6fd488c435087afa93bf10d
AKDA33 <135207294+AKDA33@users.noreply.github.com>
2026-03-12 14:06:42 +0300

    Merge pull request #105 from bounswe/feature/stub-app
    feat: Add stub-app files

--- handlers.js
+++ handlers.js
@@ -24,3 +24,33 @@ function onButton4Click() {
 function onButton5Click() {
   console.log("Button 5 clicked -- implement me!");
 }
+
+function onButton6Click() {
+  console.log("ahmet fun fact");
+    // Opening a new window for our fun fact.
+    const newWindow = window.open('', '_blank');
+    //fetch the response
+    fetch('https://uselessfacts.jsph.pl/api/v2/facts/random?language=en')
+          .then(res => res.json())
+          .then(data => {
+              newWindow.document.write(`
+                  <html>
+                  <head><title>Random Fun Fact</title></head>
+                  <body style="font-family: Arial; padding: 40px; text-align:
+  center;">
+                      <h1>Random Fun Fact</h1>
+                      <p style="font-size: 20px;">${data.text}</p>
+                      <hr>
+                      <h3>What does this API return?</h3>
+                      <p>The Useless Facts API returns a random useselss funfact
+                      from a database. Each response includes the fact
+   text,
+                      its source URL, and language info.</p>
+                      <h4>Raw API Response:</h4>
+                      <pre style="text-align:left; background:#f4f4f4;
+  padding:15px; border-radius:8px;">${JSON.stringify(data, null, 2)}</pre>
+                  </body>
+                  </html>
+              `);
+          });
+}
